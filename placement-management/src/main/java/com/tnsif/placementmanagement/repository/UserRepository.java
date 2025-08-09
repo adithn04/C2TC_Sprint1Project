643373cf411e653d5b21828f6cc15cc351e6a01a
Adithyan V <166741329+adithn04@users.noreply.github.com>
2025-08-09 22:23:37 +0530

    This file contain sprint 1 project.

--- placement-management/src/main/java/com/tnsif/placementmanagement/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.placementmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tnsif.placementmanagement.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
-
