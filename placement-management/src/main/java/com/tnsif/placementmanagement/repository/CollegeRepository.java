b8205deb1c5a1e532e67a26b81104966231537e9
Adithyan V <166741329+adithn04@users.noreply.github.com>
2025-08-09 22:22:57 +0530

    This file contain sprint 1 project.

--- placement-management/src/main/java/com/tnsif/placementmanagement/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.placementmanagement.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tnsif.placementmanagement.entity.College;
-
-public interface CollegeRepository extends JpaRepository<College, Long> {
-}
-
