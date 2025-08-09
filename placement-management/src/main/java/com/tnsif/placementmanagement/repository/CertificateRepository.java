2ce431f2487e78991cea7c607b415148d103af92
Adithyan V <166741329+adithn04@users.noreply.github.com>
2025-08-09 22:22:36 +0530

    This file contain sprint 1 project.

--- placement-management/src/main/java/com/tnsif/placementmanagement/repository/CertificateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tnsif.placementmanagement.repository;
-
-import com.tnsif.placementmanagement.entity.Certificate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertificateRepository extends JpaRepository<Certificate, Long> {
-}
